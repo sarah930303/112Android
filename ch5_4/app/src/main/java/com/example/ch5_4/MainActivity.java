63f0fc1dd2d83794b17fa66f46eed36ab04dea9d
sarah930303 <sherry93030365@gmail.com>
2024-04-12 19:23:43 +0800

    0412

--- ch5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
+++ ch5_4/app/src/main/java/com/example/ch5_4/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.ch5_4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView; // Add this line to import TextView
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            EditText txtInput = (EditText) findViewById(R.id.txtInput);
+            txtInput.setText("按下BACK按鍵");
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        TextView output = (TextView) findViewById(R.id.lblOutput); // Ensure this ID matches with your layout's TextView ID
+        output.setText("按下按鍵碼:" + keyCode);
+        return super.onKeyUp(keyCode, event);
     }
 }
